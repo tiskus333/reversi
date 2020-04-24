d63799e97f0d13cb06fc98bcbe93bd2b4b0ae262
Mateusz Chrusciel <tiskus.3009@gmail.com>
2020-04-24 10:08:02 +0200

    network game should work

---
+++ Player.java
@@ -0,0 +1,155 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Player {
+    private final int EMPTY = 0;
+    private final int WHITE = 1;
+    private final int BLACK = -1;
+    private final int BOARD_SIZE = 8;
+    public int player_turn = BLACK;
+    public int my_color;
+
+    private int board[][];
+    private int possible_moves[][];
+
+    private Socket socket;
+    private DataInputStream dataIn;
+    private DataOutputStream dataOut;
+
+    public Player() {
+        board = new int[BOARD_SIZE][BOARD_SIZE];
+        possible_moves = new int[BOARD_SIZE][BOARD_SIZE];
+    }
+
+    public void connectToServer() {
+        int tmp_id;
+        System.out.println("Connecting to server at localhost:60065");
+        try {
+            socket = new Socket("localhost", 60065);
+            dataIn = new DataInputStream(socket.getInputStream());
+            dataOut = new DataOutputStream(socket.getOutputStream());
+            tmp_id = dataIn.readInt();
+            if (tmp_id == 1) {
+                my_color = BLACK;
+                System.out.println("Connected to the server as player BLACK.");
+            } else {
+                my_color = WHITE;
+                System.out.println("Connected to the server as player WHITE.");
+            }
+        } catch (IOException e) {
+            System.out.println("IOException from ConnectToServer.");
+        }
+    }
+
+    public void requestBoardState() {
+        System.out.println("Sending request for board state.");
+        try {
+            dataOut.writeInt(my_color);
+            dataOut.flush();
+        } catch (Exception e) {
+            System.out.println("Cannot connect to the server.");
+        }
+        System.out.println("Reading board");
+        try {
+            for (int i = 0; i < BOARD_SIZE; ++i)
+                for (int j = 0; j < BOARD_SIZE; ++j) {
+                    board[i][j] = dataIn.readInt();
+                }
+        } catch (IOException e) {
+            System.out.println("IOException cannot read board state from server.");
+        }
+    }
+
+    public void requestValidMoves() {
+        System.out.println("Sending request for board state.");
+        try {
+            dataOut.writeInt(my_color);
+            dataOut.flush();
+        } catch (Exception e) {
+            System.out.println("Cannot connect to the server.");
+        }
+        System.out.println("Reading moves");
+        try {
+            for (int i = 0; i < BOARD_SIZE; ++i)
+                for (int j = 0; j < BOARD_SIZE; ++j) {
+                    possible_moves[i][j] = dataIn.readInt();
+                }
+        } catch (IOException e) {
+            System.out.println("IOException cannot read possible moves from server.");
+        }
+    }
+
+    public void displayBoard() {
+        //System.out.println("black: " + black_pices_nr + " white: " + white_pieces_nr);
+
+        System.out.println("BOARD STATE");
+        for (int i = 0; i < BOARD_SIZE; ++i) {
+            for (int j = 0; j < BOARD_SIZE; ++j) {
+                System.out.format("%3d", board[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void displayValidMoves() {
+        //System.out.println("black: " + black_pices_nr + " white: " + white_pieces_nr);
+
+        System.out.println("POSSIBLE MOVES");
+        for (int i = 0; i < BOARD_SIZE; ++i) {
+            for (int j = 0; j < BOARD_SIZE; ++j) {
+                System.out.format("%3d", possible_moves[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void move(int x, int y) {
+
+        try {
+            dataOut.writeInt(x);
+            dataOut.writeInt(y);
+            dataOut.flush();
+            player_turn *= -1;
+        } catch (IOException e) {
+            System.out.println("IOException, cannot send move to server.");
+        }
+
+    }
+
+    public void debug_move() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input 2 cooardinates");
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        if (possible_moves[x][y] == 1)
+            move(x, y);
+
+    }
+
+    public void waitForTurn() {
+        try {
+            player_turn = dataIn.readInt();
+            System.out.println("My turn " + (player_turn == my_color));
+        } catch (IOException e) {
+        }
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.connectToServer();
+        while (true) {
+            if (player.my_color == player.player_turn) {
+                player.requestBoardState();
+                player.displayBoard();
+                player.requestValidMoves();
+                player.displayValidMoves();
+                player.debug_move();
+            } else
+                player.waitForTurn();
+        }
+        //player.move(0, 0);
+    }
+}
--- Server.java
+++ Server.java
@@ -1,18 +1,26 @@
+import java.io.*;
+import java.net.*;
+
 /**
  * Server
  */
 public class Server {
 
-    public static final int EMPTY = 0;
-    public static final int WHITE = 1;
-    public static final int BLACK = -1;
-    public static final int BOARD_SIZE = 8;
+    public final int EMPTY = 0;
+    public final int WHITE = 1;
+    public final int BLACK = -1;
+    public final int BOARD_SIZE = 8;
 
-    public static int players_turn = BLACK;
-    public static int white_pieces_nr = 2;
-    public static int black_pices_nr = 2;
-    public static int player_won = 0;
+    public int player_turn = BLACK;
+    public int white_pieces_nr = 2;
+    public int black_pices_nr = 2;
+    public int player_won;
+    private int player_nr;
+    private boolean is_possible_move;
 
+    private ServerSocket ss;
+    private Socket s;
+    private ServerConnection player_black, player_white;
     /**
      *    INITIAL BOARD PLACEMENT
      *       0  1  2  3  4  5  6  7
@@ -25,17 +33,203 @@ public class Server {
      * 6     _  _  _  _  _  _  _  _
      * 7     _  _  _  _  _  _  _  _
      */
-    private static int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
+    private int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
+    private int[][] possible_moves = new int[BOARD_SIZE][BOARD_SIZE];
 
-    public static int[][] getValidPositions(int player) {
-        int[][] validPositions = new int[BOARD_SIZE][BOARD_SIZE];
-        for (int i = 0; i < BOARD_SIZE; ++i)
-            for (int j = 0; j < BOARD_SIZE; ++j)
-                validPositions[i][j] = isValidPosition(i, j, player);
-        return validPositions;
+    public Server() {
+        System.out.println("Started game server.");
+        player_nr = 0;
+        try {
+            ss = new ServerSocket(60065);
+        } catch (IOException e) {
+            System.out.println("IOExcpetion from Server() constructor.");
+        }
     }
 
-    public static int isValidPosition(int x, int y, int player) {
+    public void acceptConnections() {
+        try {
+            System.out.println("Waiting for players to join.");
+            while (player_nr < 2) {
+
+                s = ss.accept();
+                ++player_nr;
+                System.out.println("Player #" + player_nr + " connceted.");
+                ServerConnection sc = new ServerConnection(s, player_nr);
+                if (player_nr == 1)
+                    player_black = sc;
+                else
+                    player_white = sc;
+            }
+            System.out.println("Both plawes connected. No longer accepting new players.");
+        } catch (IOException e) {
+            System.out.println("IOExcpetion from acceptConnection().");
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void runGame() {
+        Thread thread_black = new Thread(player_black);
+        Thread thread_white = new Thread(player_white);
+        thread_black.start();
+        thread_white.start();
+    }
+
+    private class ServerConnection implements Runnable {
+
+        private Socket socket;
+        private DataInputStream dataIn;
+        private DataOutputStream dataOut;
+        private int player_id;
+
+        public ServerConnection(Socket s, int id) {
+            socket = s;
+            if (id == 1)
+                player_id = BLACK;
+            else
+                player_id = WHITE;
+            try {
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+                dataOut.writeInt(id);
+                dataOut.flush();
+            } catch (Exception e) {
+                System.out.println("IOException from ServerConnection() constructor.");
+            }
+        }
+
+        private void sendBoardState() {
+            // while (player_id != player_turn);
+            System.out.println("Waiting for player to send board state to.");
+            try {
+                dataIn.readInt();
+            } catch (Exception e) {
+                System.out.println("Cannot connect to player #" + player_id);
+            }
+
+            try {
+                System.out.println("Sending board to: " + player_turn);
+                for (int i = 0; i < BOARD_SIZE; ++i)
+                    for (int j = 0; j < BOARD_SIZE; ++j) {
+                        dataOut.writeInt(board[i][j]);
+                    }
+                dataOut.flush();
+            } catch (IOException e) {
+                System.out.println("IOException, cannot send board to client#" + player_id);
+            }
+        }
+
+        private void sendValidMoves() {
+            //while (player_id != player_turn);
+            System.out.println("Waiting for player to send possible moves to.");
+            try {
+                dataIn.readInt();
+            } catch (Exception e) {
+                System.out.println("Cannot connect to player #" + player_id);
+            }
+            try {
+                System.out.println("Sending moves to: " + player_turn);
+                for (int i = 0; i < BOARD_SIZE; ++i)
+                    for (int j = 0; j < BOARD_SIZE; ++j) {
+                        dataOut.writeInt(possible_moves[i][j]);
+                    }
+                dataOut.flush();
+            } catch (IOException e) {
+                System.out.println("IOException, cannot send possible moves to client #" + player_id);
+            }
+        }
+
+        private void recieveMove() {
+            try {
+                int x = -1, y = -1;
+                x = dataIn.readInt();
+                y = dataIn.readInt();
+                System.out.println("Recieved move " + x + ":" + y);
+                move(x, y, player_id);
+                //switchPlayers();
+                displayBoard();
+            } catch (IOException e) {
+                System.out.println("Cannot read move from player #" + player_id);
+            }
+        }
+
+        private void switchPlayers() {
+            player_turn *= -1;
+
+        }
+
+        private void notifyEnemy() {
+            try {
+                switchPlayers();
+                System.out.println("player " + (player_id) + " fnished");
+                player_black.dataOut.writeInt(player_turn);
+                player_black.dataOut.flush();
+                player_white.dataOut.writeInt(player_turn);
+                player_white.dataOut.flush();
+
+            } catch (Exception e) {
+                System.out.println("IOException, cannot notify players");
+            }
+        }
+
+        private void waitForEnemy() {
+            try {
+                player_turn = dataIn.readInt();
+            } catch (IOException e) {
+                System.out.println("IOException, cannot read player turn");
+            }
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                // System.out.println(player_id + "" + player_turn);
+                //if (player_id == player_turn) {
+                System.out.println("From player #" + player_id + "Now plays: " + player_turn);
+                sendBoardState();
+                getValidPositions(player_id);
+                if (is_possible_move) {
+                    sendValidMoves();
+                    recieveMove();
+                }
+                notifyEnemy();
+                //} else
+                //  waitForEnemy();
+            }
+        }
+
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * @param player
+     * @return
+     */
+
+    public int[][] getValidPositions(int player) {
+        int tmp_move;
+        is_possible_move = false;
+        for (int i = 0; i < BOARD_SIZE; ++i)
+            for (int j = 0; j < BOARD_SIZE; ++j) {
+                tmp_move = isValidPosition(i, j, player);
+                if (tmp_move == 1)
+                    is_possible_move = true;
+                possible_moves[i][j] = tmp_move;
+            }
+        return possible_moves;
+    }
+
+    public int isValidPosition(int x, int y, int player) {
         boolean isValid = false;
 
         if (board[x][y] != EMPTY)
@@ -58,10 +252,9 @@ public class Server {
             return 1;
         else
             return 0;
-
     }
 
-    public static boolean checkPosition(int x, int y, int dx, int dy, int player, boolean possible) {
+    public boolean checkPosition(int x, int y, int dx, int dy, int player, boolean possible) {
         if (x >= BOARD_SIZE || x < 0 || y >= BOARD_SIZE || y < 0)
             return false;
         if (board[x][y] == EMPTY)
@@ -73,7 +266,7 @@ public class Server {
         return checkPosition(x + dx, y + dy, dx, dy, player, possible);
     }
 
-    public static void initBoard() {
+    public void initBoard() {
         for (int i = 0; i < BOARD_SIZE; ++i)
             for (int j = 0; j < BOARD_SIZE; ++j)
                 board[i][j] = EMPTY;
@@ -82,9 +275,10 @@ public class Server {
         board[3][4] = board[4][3] = BLACK;
     }
 
-    public static void move(int x, int y, int player) {
-        int valid_positions[][] = getValidPositions(player);
-        if (valid_positions[x][y] == 1) {
+    public void move(int x, int y, int player) {
+        if (x < 0 || x > BOARD_SIZE || y < 0 || y > BOARD_SIZE)
+            return;
+        if (possible_moves[x][y] == 1) {
             board[x][y] = player;
             if (player == WHITE)
                 ++white_pieces_nr;
@@ -92,10 +286,10 @@ public class Server {
                 ++black_pices_nr;
             updateBoard(x, y, player);
         } else
-            System.out.println("INCORECT PLACEMNET!!!");
+            System.out.println("INCORECT PLACEMENT!!!");
     }
 
-    public static void updateBoard(int x, int y, int player) {
+    public void updateBoard(int x, int y, int player) {
         update(x + 1, y, 1, 0, player); // ee
         update(x - 1, y, -1, 0, player); // ww
         update(x, y + 1, 0, 1, player); // nn
@@ -106,7 +300,7 @@ public class Server {
         update(x - 1, y - 1, -1, -1, player); // wn
     }
 
-    public static boolean update(int x, int y, int dx, int dy, int player) {
+    public boolean update(int x, int y, int dx, int dy, int player) {
         if (x >= BOARD_SIZE || x < 0 || y >= BOARD_SIZE || y < 0)
             return false;
         if (board[x][y] == EMPTY)
@@ -122,7 +316,7 @@ public class Server {
         return false;
     }
 
-    public static void displayBoard() {
+    public void displayBoard() {
         System.out.println("black: " + black_pices_nr + " white: " + white_pieces_nr);
         for (int i = 0; i < BOARD_SIZE; ++i) {
             for (int j = 0; j < BOARD_SIZE; ++j) {
@@ -130,35 +324,46 @@ public class Server {
             }
             System.out.println();
         }
-
     }
 
-    public static void displayPossiblePositions(int player) {
-        int tab[][] = getValidPositions(player);
+    public void displayPossiblePositions(int player) {
         System.out.print("VALID POSITIONS FOR ");
-        if (player == 1)
+        if (player == WHITE)
             System.out.println("WHITE:");
         else
             System.out.println("BLACK:");
         for (int i = 0; i < BOARD_SIZE; ++i) {
             for (int j = 0; j < BOARD_SIZE; ++j) {
-                System.out.format("%3d", tab[i][j]);
+                System.out.format("%3d", possible_moves[i][j]);
             }
             System.out.println();
         }
     }
 
-    public static void main(String[] args) {
+    public int[] getScore() {
+        int score[] = new int[2];
+        score[0] = white_pieces_nr;
+        score[1] = black_pices_nr;
+        return score;
+    }
 
-        initBoard();
-        displayBoard();
-        displayPossiblePositions(BLACK);
-        move(2, 3, BLACK);
-        displayBoard();
-        displayPossiblePositions(WHITE);
-        move(4, 2, WHITE);
-        displayBoard();
-        displayPossiblePositions(BLACK);
-        move(0, 0, WHITE);
+    public static void main(String[] args) {
+        // Window window = new Window();
+        // window.drawBoard();
+        Server server = new Server();
+        server.initBoard();
+        server.acceptConnections();
+        server.runGame();
+
+        server.closeConnection();
+        // displayBoard();
+        // displayPossiblePositions(BLACK);
+        // move(2, 3, BLACK);
+        // displayBoard();
+        // displayPossiblePositions(WHITE);
+        // move(4, 2, WHITE);
+        // displayBoard();
+        // displayPossiblePositions(BLACK);
+        // move(0, 0, WHITE);
     }
 }
